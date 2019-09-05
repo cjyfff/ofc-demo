24b58f90cdfc4332a622542f93467c64c98beb7f
cjyfff <631127458@qq.com>
2019-09-05 18:08:08 +0800

    init

---
+++ ofc/src/main/java/com/cjyfff/ofc/OfcApplication.java
@@ -0,0 +1,13 @@
+package com.cjyfff.ofc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfcApplication.class, args);
+    }
+
+}
